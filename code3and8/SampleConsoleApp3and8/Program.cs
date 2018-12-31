17f81f495a225833d4a511d5fa17c0804951adf9
Jason Cable <jason@jasoncable.com>
2018-12-31 06:54:21 -0500

    start of C#8/.NETCore3 project

---
+++ code3and8/SampleConsoleApp3and8/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SampleConsoleApp3and8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string s = default;
+            string s1 = default(string);
+            int i = default;
+
+            Console.WriteLine("DONE!");
+            Console.ReadLine();
+        }
+    }
+}
