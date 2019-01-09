aba16b1f22687cb6766e38a532e2af8281bc5b98
Jason Cable <jason@jasoncable.com>
2019-01-09 17:58:51 -0500

    C# 8 method parameter tests

---
+++ code3and8/SampleConsoleApp3and8/MethodTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleConsoleApp3and8
+{
+    public class MethodTests
+    {
+        public void Method1(string s = "string") { }
+
+        public void Method2(string s = "1", int i = 1) { }
+
+        public void Method3(
+            string a = "one",
+            int b = new Int32(),
+            string c = "",
+            string d = default(string),
+            string e = default,
+            string f = null,
+            int? g = null,
+            int h = default(int),
+            int i = default,
+            int? j = default(int?),
+            int? k = default
+            ) {}
+    }
+}
